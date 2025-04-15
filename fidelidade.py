bff7dfe24d12b757fda533fdff7366c84ec85ec5
GersonSilvaa <gerson.silva97@gmail.com>
2025-04-15 15:02:47 +0100

    Update fidelidade.py

--- fidelidade.py
+++ fidelidade.py
@@ -5,12 +5,23 @@ from utils import processar_ficheiro
 def run_fidelidade():
     st.title("Gestão de Faturação - FIDELIDADE 🛡️")
 
-    st.subheader("Upload do Ficheiro de Comparação")
-    uploaded_file = st.file_uploader("Ficheiro de comparação (FIDELIDADE)", type=["xlsx"], key="fidelidade_comparacao")
+    st.subheader("Upload do Ficheiro Exportado do Gestow")
+    uploaded_file = st.file_uploader("Ficheiro de serviços da Fidelidade (Gestow)", type=["xlsx"], key="fidelidade_gestow")
 
     if uploaded_file:
-        df = processar_ficheiro(uploaded_file)
+        df = processar_ficheiro(
+            uploaded_file,
+            colunas_obrigatorias=[
+                "Matrícula",
+                "Marca",
+                "Modelo",
+                "Categoria de Veículo",
+                "KMS a Faturar no Serviço",
+                "Valor a Faturar S/IVA"
+            ]
+        )
+
         if df is not None:
-            st.success("Ficheiro da Fidelidade carregado com sucesso!")
-            st.write("Pré-visualização dos dados:")
+            st.success("Ficheiro carregado com sucesso!")
+            st.write("Pré-visualização dos dados importados:")
             st.dataframe(df.head())
