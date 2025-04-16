08c5eac24f19409486c806aa60f512e37e67565a
GersonSilvaa <gerson.silva97@gmail.com>
2025-04-16 15:37:10 +0100

    Update acp.py

--- acp.py
+++ acp.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+from utils import processar_ficheiro
+# import outros helpers no futuro conforme precisares
 
+def run_acp():
+    st.title("Gestão de Faturação - ACP 🛻")
+
+    st.subheader("Upload do Ficheiro Exportado do Gestow")
+    uploaded_file = st.file_uploader("Ficheiro de serviços da ACP (Gestow)", type=["xlsx"], key="acp_gestow")
+
+    if uploaded_file:
+        df = processar_ficheiro(uploaded_file)
+
+        if df is not None:
+            st.success("Ficheiro carregado com sucesso!")
+            st.write("Pré-visualização dos dados:")
+            st.dataframe(df, height=600)
