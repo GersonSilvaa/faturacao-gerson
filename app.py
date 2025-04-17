b24dd48932429dbc3a5dd5aa394ec1021bd9d071
GersonSilvaa <gerson.silva97@gmail.com>
2025-04-17 09:10:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from acp import run_acp
 
 # ----- Layout da página -----
 st.set_page_config(
-page_title="Gestão de Faturação",
-layout="wide"
+    page_title="Gestão de Faturação",
+    layout="wide"
 )
 
 # ----- Login e Navegação -----
@@ -18,24 +18,25 @@ if 'login' not in st.session_state:
 if not st.session_state['login']:
     st.subheader("Login de Acesso")
     verificar_login()
-else: # ----- Sidebar com logo e menu -----
+else:
+    # ----- Sidebar com logo e menu -----
     try:
         logo = Image.open("assets/logo.png")  # Altera o caminho se estiver noutra pasta
         st.sidebar.image(logo, use_container_width=True)
     except:
         st.sidebar.write("🧾 Logo não encontrado.")
 
-st.sidebar.title("Menu")
-st.sidebar.markdown(f"👤 Utilizador: **{st.session_state.get('utilizador', 'Desconhecido')}**")
-companhia = st.sidebar.selectbox("Companhia:", ["IPA", "FIDELIDADE", "ACP"], key="companhia")
+    st.sidebar.title("Menu")
+    st.sidebar.markdown(f"👤 Utilizador: **{st.session_state.get('utilizador', 'Desconhecido')}**")
+    companhia = st.sidebar.selectbox("Companhia:", ["IPA", "FIDELIDADE", "ACP"], key="companhia")
 
-# ----- Título da app -----
-st.title("Gestão de Faturação")
+    # ----- Título da app -----
+    st.title("Gestão de Faturação")
 
-# ----- Chamar o módulo da companhia selecionada -----
-if companhia == "IPA":
-    run_ipa()
-elif companhia == "FIDELIDADE":
-    run_fidelidade()
-elif companhia == "ACP":
-    run_acp()
+    # ----- Chamar o módulo da companhia selecionada -----
+    if companhia == "IPA":
+        run_ipa()
+    elif companhia == "FIDELIDADE":
+        run_fidelidade()
+    elif companhia == "ACP":
+        run_acp()
